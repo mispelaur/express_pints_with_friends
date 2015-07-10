58564934001abb3652cca8a07ddb01ff773bc7f8
Lauren Spencer <spencer.lauren.m@gmail.com>
2015-07-10 20:10:08 +0100

    added jquery locally

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 require.config({
   paths: {
-    "jquery": "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min",
+    "jquery": "lib/jquery",
     "underscore": "lib/underscore"
   }
 })
